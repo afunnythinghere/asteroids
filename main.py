eb84ffd210e448a23425902fcf8d4ca504bd86ad
afunnythinghere <cp1995726@gmail.com>
2025-12-21 21:36:36 -0500

    update ... added asteroids and field spawn

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,7 +12,11 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
 
@@ -23,11 +29,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        for player in drawable:
-            player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        for player in updatable:
-            player.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
         milisec = clock.tick(60)
         dt = milisec/1000
         
